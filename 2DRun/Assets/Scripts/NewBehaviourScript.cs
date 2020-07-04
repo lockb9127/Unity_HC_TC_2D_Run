1a58eb80bc5ae3a0f266faa4472cf595a92df95b
apqdy4944asd <lockb9127@gmail.com>
2020-07-04 21:00:55 +0800

    程式

---
+++ 2DRun/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Start()
+    {
+        //靜態成員用法
+        //靜態屬性Property = 欄位 Field (儲存資料)
+        //語法:類別名稱，靜態屬性名稱
+        print(Mathf.PI);
+        //取得100 - 500 隨機
+        print(Random.Range(100,501);
+}
---
+++ 2DRun/Assets/Scripts/player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //單行註解
+    /*多行註解
+     * 
+     * 
+     * */
+
+    #region
+    
+
+    [Header("角色")]
+    public int blood = 99;
+    [Range(10,49.9f)]
+    public float jump = 49.9f;
+    public int bulict = 10;
+    [Range(10, 29.9f)]
+    public float speed = 29.9f;
+    public AudioClip jumpsound;
+    public AudioClip shootsound;
+    public bool dear;
+
+
+    #endregion
+    #region
+    private void Jump()
+    {
+        print("跳躍");
+    }
+    #endregion
+
+    #region 事件區域
+    //開始 start
+    //播放遊戲時執行一次
+    //初始化:
+    #region
+    private void slide()
+    {
+        print("滑行");
+    }
+    #endregion
+    private void Start()
+    {
+        //呼叫跳躍方法
+        Jump();
+    }
+    //更新Update
+    //播放遊戲一秒執行約60次-60FPS
+    //移動，監聽玩家鍵盤，滑鼠與觸控
+    private void Update()
+    {
+        slide();
+    }
+    #endregion
+}
+
+
+
+
